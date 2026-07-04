bea2c4995b3df0ec680cf8378bdf24fd754de47a
Vorssaint <vorssaint@gmail.com>
2026-07-04 02:13:46 -0300

    vorssaint 3.1.7: quick panel, Dock click to minimize, middle click, clipboard images and files, new quick tools

--- Casks/vorssaint.rb
+++ Casks/vorssaint.rb
@@ -1,6 +1,6 @@
 cask "vorssaint" do
-  version "3.1.5"
-  sha256 "78619e6c0aadb6b8b058c40b5df1398da292bfa05db6fa32fd843d87e5d0c0f4"
+  version "3.1.7"
+  sha256 "2d0b01c728605179b1df32c2cc33bda94922c4389c18c02f4b9ddef9d8269b7d"
 
   url "https://github.com/vorssaint/vorssaint-utils/releases/download/v#{version}/Vorssaint-#{version}.dmg"
   name "Vorssaint"
